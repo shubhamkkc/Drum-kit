21161e08023142a1bcbbef96c0358580e745047e
Shubham Krishna <shubhamkrishnasound@gmail.com>
2021-12-23 01:02:55 +0530

    text color

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
 
-        this.style.color = "grey";
+    
         var buttonInnerHtml = this.innerHTML;
         makeSound(buttonInnerHtml);
         buttonAnimation(buttonInnerHtml);
